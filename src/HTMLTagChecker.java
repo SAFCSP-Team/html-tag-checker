fcb51887f1b08378457ee0b09f9a43cd7204334d
nourabyte <148945652+nourabyte@users.noreply.github.com>
2024-08-29 15:53:20 +0300

    Create HTMLTagChecker.java

---
+++ src/HTMLTagChecker.java
@@ -0,0 +1,25 @@
+class HTMLTagChecker {
+   
+    public static void main(String[] args) {
+     /* write your code here */
+
+      
+    }
+
+  private static void readFile{
+    /* write your code here */
+    
+  }
+
+  private static Tag Tag() {
+    /* write your code here */
+  
+  }
+
+   private boolean Checker() {
+    /* write your code here */
+     
+   }
+  
+  
+}
